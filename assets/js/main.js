f235e3806c1d59afb96778e76d03ee5f3a6465bc
auyadhu <yadhuau@gmail.com>
2015-05-26 22:20:54 +0530

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+	// Preloader
+
+//<![CDATA[
+        $(window).load(function() { // makes sure the whole site is loaded
+            $('#status').fadeOut(); // will first fade out the loading animation
+            $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+            $('body').delay(350).css({'overflow':'visible'});
+        })
+    //]]>
+		
+	$('a.gallery').nivoLightbox({
+    effect: 'fade',                             // The effect to use when showing the lightbox
+    theme: 'default',                           // The lightbox theme to use
+    keyboardNav: true,                          // Enable/Disable keyboard navigation (left/right/escape)
+    clickOverlayToClose: true,                  // If false clicking the "close" button will be the only way to close the lightbox
+    onInit: function(){},                       // Callback when lightbox has loaded
+    beforeShowLightbox: function(){},           // Callback before the lightbox is shown
+    afterShowLightbox: function(lightbox){},    // Callback after the lightbox is shown
+    beforeHideLightbox: function(){},           // Callback before the lightbox is hidden
+    afterHideLightbox: function(){},            // Callback after the lightbox is hidden
+    onPrev: function(element){},                // Callback when the lightbox gallery goes to previous item
+    onNext: function(element){},                // Callback when the lightbox gallery goes to next item
+    errorMessage: 'The requested content cannot be loaded. Please try again later.' // Error message when content can't be loaded
+});
